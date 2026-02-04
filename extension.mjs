e27f888a61b049dec00212476f384476b9af2e12
lin3939 <256104980+lin3939@users.noreply.github.com>
2026-02-04 22:50:12 +0800

    feat: add local AI plan Now only supports OpenAI API, so must use baseURL that supports OpenAI API.

--- extension.mjs
+++ extension.mjs
@@ -2,11 +2,14 @@ import * as vscode from 'vscode';
 import OpenAI from 'openai';
 import * as userdataUtils from './utils/userdata-utils.mjs';
 import * as userContext from './utils/get-context-utils.mjs';
-import * as responseUtils from './utils/response-utils.mjs'
+import * as responseUtils from './utils/response-utils.mjs';
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+
+
 
 // Main function to process user request through OpenAI API.
 // Returns Object of necessary information of AI's response on success, false on error.
@@ -23,7 +26,6 @@ async function main(context) {
                     {"role": "system", "content": userdataUtils.getSysPrompt(context.extensionPath).system_prompt},
                     {"role": "user", "content": userContext.getContext().content}
                 ], 
-                // Temporarily disable streaming to fix token usage issue in case of unsupported models.
                 // stream: false,
                 // stream_options: {include_usage: true}
             });
@@ -41,8 +43,44 @@ async function main(context) {
     } catch (error) {
         vscode.window.showErrorMessage("No file opened." + String(error));
         return false;
+    }    
+}
+
+// Main function to process user request through ollama's OpenAI API capability.
+// Returns Object of necessary information of AI's response on success, false on error.
+async function ollama_main(context) {
+    try {
+        const user_data = userdataUtils.getLocalPlanData(context.extensionPath);
+        const openai = new OpenAI({apiKey: "not-needed",
+            baseURL: user_data.baseURL
+        });
+        if (user_data.baseURL && user_data.model){
+            let completion = await openai.chat.completions.create({
+                model: user_data.model,
+                messages: [
+                    {"role": "system", "content": userdataUtils.getSysPrompt(context.extensionPath).system_prompt},
+                    {"role": "user", "content": userContext.getContext().content}
+                ], 
+                // stream: false,
+                // stream_options: {include_usage: true}
+            });
+            return {
+                "date": new Date().toLocaleString(), 
+                "file": userContext.getContext().fileName, 
+                "model": user_data.model,
+                "usage": completion.usage.total_tokens,
+                "response": completion.choices[0].message.content
+            };
+        }else{
+            await userdataUtils.writeData(context, true);
+            return false;
+        }
+    } catch (error) {
+        vscode.window.showErrorMessage("Ollama Error: " + String(error));
+        return false;
     }
 }
+
 
 //  Registers the following commands:
 //  'pcpr.checkCode': Checks code that user is editing for logical errors using AI.Modify configuration if configration is not usable.
@@ -82,18 +120,60 @@ export function activate(context) {
 
 	})
 	context.subscriptions.push(checkCode);
+    
+	const localCheck = vscode.commands.registerCommand('pcpr.localCheck', async function(){
+        try{
+            const user_data = userdataUtils.getLocalPlanData(context.extensionPath);
+            if (user_data.baseURL && user_data.model){
+                const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+                statusBar.text = "$(sync-spin)PCPR: Local Analying....";
+                statusBar.show();
+
+                ollama_main(context).then((result)=>{
+                    if (result){
+                        responseUtils.showResponse(result).then((err) => {
+                            if (err) {
+                                return;
+                            }   
+                        }).catch((err)=>{
+                            vscode.window.showErrorMessage(`show Error: ${String(err)}`);
+                        });
+                    }else{
+                        vscode.window.showErrorMessage("Something went wrong, check your config or status.");
+                    }
+                    statusBar.hide();
+                });
+
+            }else{
+                await userdataUtils.writeData(context, true);
+            }
+        }catch(err){
+            vscode.window.showErrorMessage(String(err));
+        }
+
+	})
+	context.subscriptions.push(localCheck);
 
 
     const modifyConfig = vscode.commands.registerCommand('pcpr.modifyConfig', async function(){
-        userdataUtils.writeData(context);
+        await userdataUtils.writeData(context);
     })
     context.subscriptions.push(modifyConfig);
+
+    const modifyLocalConfig = vscode.commands.registerCommand('pcpr.modifyLocalConfig', async function(){
+        await userdataUtils.writeData(context, true);
+    })
+    context.subscriptions.push(modifyLocalConfig);
 
 
     const changeModel = vscode.commands.registerCommand('pcpr.changeModel', async function(){
         userdataUtils.changeModel(context);
     })
     context.subscriptions.push(changeModel);
+    const changeLocalModel = vscode.commands.registerCommand('pcpr.changeLocalModel', async function(){
+        userdataUtils.changeModel(context, true);
+    })
+    context.subscriptions.push(changeLocalModel);
 }
 
 export function deactivate() {}
--- utils/userdata-utils.mjs
+++ utils/userdata-utils.mjs
@@ -37,7 +37,6 @@ function _decrypt(encryptedText){
 // Returns Object of configuration on success, fasle on error.
 export function getData(extensionPath){
     let filePath = path.join(extensionPath, "./config/.user_config");
-    console.log(filePath)
     try{
         return JSON.parse(_decrypt(fs.readFileSync(filePath, "utf-8"), extensionPath));
     }catch(err){
@@ -46,60 +45,118 @@ export function getData(extensionPath){
 }
 
 // Edits configuration including baseURL, api and model.
-// Configuration will be saved after encryption.
+// Configuration will be saved after encryption, but local plan configuration will not be encrypted.
 // Returns true on success, fasle on error.
-export async function writeData(context){
-    let filePath = path.join(context.extensionPath, "./config/.user_config");
-    try{
-        let base = await vscode.window.showInputBox({
-            prompt: "baseURL", 
-            placeHolder: "Your baseURL provided by AI provider",
-            ignoreFocusOut: true,
-            password: false
-        });
-        let ak = await vscode.window.showInputBox({
-            prompt: "API Key", 
-            placeHolder: "Your api-key provided by AI provider", 
-            ignoreFocusOut: true,
-            password: true
-        });
-        let model = await vscode.window.showInputBox({
-            prompt: "AI Model", 
-            placeHolder: "The AI model your want to use",
-            ignoreFocusOut: true, 
-            password: false
-        });
-        let payload = JSON.stringify({"baseURL": base,"apiKey": ak, "model": model}, null, 4);
-        let encrypted_payload = _encrypt(payload);
-        fs.writeFileSync(filePath, encrypted_payload);
-        vscode.window.showInformationMessage("Config saved successfully.");
-        return true;
-    }catch(err){
-        vscode.window.showErrorMessage("Config Save Error: " + String(err));
-        return false;
+export async function writeData(context, local = false){
+    if (!local){
+        const filePath = path.join(context.extensionPath, "./config/.user_config");
+        try{
+            let base = await vscode.window.showInputBox({
+                prompt: "baseURL", 
+                placeHolder: "Your baseURL provided by AI provider",
+                ignoreFocusOut: true,
+                password: false
+            });
+            let ak = await vscode.window.showInputBox({
+                prompt: "API Key", 
+                placeHolder: "Your api-key provided by AI provider", 
+                ignoreFocusOut: true,
+                password: true
+            });
+            let model = await vscode.window.showInputBox({
+                prompt: "AI Model", 
+                placeHolder: "The AI model your want to use",
+                ignoreFocusOut: true, 
+                password: false
+            });
+            let payload = JSON.stringify({"baseURL": base,"apiKey": ak, "model": model}, null, 4);
+            let encrypted_payload = _encrypt(payload);
+            fs.writeFileSync(filePath, encrypted_payload);
+            vscode.window.showInformationMessage("Config saved successfully.");
+            return true;
+        }catch(err){
+            vscode.window.showErrorMessage("Config Save Error: " + String(err));
+            return false;
+        }
+    }else{
+        const filePath = path.join(context.extensionPath, "./config/local_plan_config.json");
+        try{
+            let base = await vscode.window.showInputBox({
+                prompt: "baseURL(Local)", 
+                placeHolder: "Your baseURL provided by AI provider",
+                // default value is ollama's OpenAI API compatibility endpoint
+                value: "http://localhost:11434/v1/",
+                ignoreFocusOut: true,
+                password: false
+            });
+            let model = await vscode.window.showInputBox({
+                prompt: "AI Model(Local)", 
+                placeHolder: "The AI model your want to use",
+                ignoreFocusOut: true, 
+                password: false
+            });
+            let payload = JSON.stringify({"baseURL": base, "model": model}, null, 4);
+            fs.writeFileSync(filePath, payload);
+            vscode.window.showInformationMessage("Config saved successfully.");
+            return true;
+        }catch(err){
+            vscode.window.showErrorMessage("Config Save Error: " + String(err));
+            return false;
+        }
     }
 }
 
 // Only changes AI model in configuration.
 // Returns true on success, fasle on error.
-export async function changeModel(context){
-    let filePath = path.join(context.extensionPath, "./config/.user_config");
+export async function changeModel(context, local = false){
+    if (!local){
+        let filePath = path.join(context.extensionPath, "./config/.user_config");
+        try{
+            let model = await vscode.window.showInputBox({
+                prompt: "AI Model", 
+                placeHolder: "The AI model your want to use",
+                ignoreFocusOut: true, 
+                password: false
+            });
+            let user_data = getData(context.extensionPath);
+            user_data.model = model;
+            let payload = JSON.stringify(user_data, null, 4);
+            let encrypted_payload = _encrypt(payload);
+            fs.writeFileSync(filePath, encrypted_payload);
+            vscode.window.showInformationMessage("Model changed successfully.");
+            return true;
+        }catch(err){
+            vscode.window.showErrorMessage("Model change error: " + String(err));
+            return false;
+        }
+    }else{
+        let filePath = path.join(context.extensionPath, "./config/local_plan_config.json");
+        try{
+            let model = await vscode.window.showInputBox({
+                prompt: "AI Model(Local)", 
+                placeHolder: "The AI model your want to use",
+                ignoreFocusOut: true, 
+                password: false
+            });
+            let user_data = getLocalPlanData(context.extensionPath);
+            user_data.model = model;
+            let payload = JSON.stringify(user_data, null, 4);
+            fs.writeFileSync(filePath, payload);
+            vscode.window.showInformationMessage("Local Model changed successfully.");
+            return true;
+        }catch(err){
+            vscode.window.showErrorMessage("Model change error: " + String(err));
+            return false;
+        }
+    }
+}
+
+export function getLocalPlanData(extensionPath){
+    let filePath = path.join(extensionPath, "./config/local_plan_config.json");
     try{
-        let model = await vscode.window.showInputBox({
-            prompt: "", 
-            placeHolder: "The AI model your want to use",
-            ignoreFocusOut: true, 
-            password: false
-        });
-        let user_data = getData(context.extensionPath);
-        user_data.model = model;
-        let payload = JSON.stringify(user_data, null, 4);
-        let encrypted_payload = _encrypt(payload);
-        fs.writeFileSync(filePath, encrypted_payload);
-        vscode.window.showInformationMessage("Model changed successfully.");
-        return true;
+        return JSON.parse(fs.readFileSync(filePath, "utf-8"), extensionPath);
     }catch(err){
-        vscode.window.showErrorMessage("Model change error: " + String(err));
+        vscode.window.showErrorMessage("getLocalError" + String(err))
         return false;
     }
 }
